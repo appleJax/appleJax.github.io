5dcfc65567acf0752e6cf82a6aab2bdc7644155c
appleJax <appleJax@users.noreply.github.com>
2018-10-11 15:04:12 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Kevin Brewer
-### Software Engineer
-#### portfolio website
--- src/utils.js
+++
@@ -1,6 +0,0 @@
-export const slugify = (name) =>
-  name
-    .replace(/\s+/g, '_')
-    .replace(/\W/g, '')
-    .replace(/_/g, '-')
-    .toLowerCase()
