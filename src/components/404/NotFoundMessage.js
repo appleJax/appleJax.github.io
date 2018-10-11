6cb691defa84da41cb0fa798c829eca3438ffa9b
appleJax <appleJax@users.noreply.github.com>
2018-10-11 15:03:36 -0500

    fix window reference issue

--- src/components/404/NotFoundMessage.js
+++ src/components/404/NotFoundMessage.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Classes from '../../styles/classes'
 import Colors from '../../styles/colors'
 import Typography from '@material-ui/core/Typography'
 
-const NotFoundMessage = ({ classes }) =>
-  <div className={classes.container}>
-    <Typography variant='title' className={classes.main}>
-      404
-    </Typography>
-    <Typography variant='headline' className={classes.msg}>
-      Sorry, the route <code className={classes.path}>{window.location.pathname}</code> doesn't exist
-    </Typography>
-  </div>
+class NotFoundMessage extends Component {
+  constructor() {
+    super()
+
+    this.state = { path: '/404' }
+  }
+
+  componentDidMount() {
+    this.setState({ path: window.location.pathname })
+
+  }
+
+  render() {
+    const { classes } = this.props
+
+    return (
+      <div className={classes.container}>
+        <Typography variant='title' className={classes.main}>
+          404
+        </Typography>
+        <Typography variant='headline' className={classes.msg}>
+          Sorry, the route <code className={classes.path}>{this.state.path}</code> doesn't exist
+        </Typography>
+      </div>
+    )
+  }
+}
 
 const styles = (theme) => ({
   container: {
