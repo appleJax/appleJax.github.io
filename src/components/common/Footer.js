cd8b36632ecd88127c52180b3f7d5ab53e335818
appleJax <appleJax@users.noreply.github.com>
2023-03-06 15:55:34 -0800

    update bio and footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import Colors from 'Styles/colors'
-import Classes from 'Styles/classes'
-import SocialLink from './SocialLink'
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Colors from "Styles/colors";
+import Classes from "Styles/classes";
+import SocialLink from "./SocialLink";
 
-const Footer = ({ classes }) =>
+const Footer = ({ classes }) => (
   <div className={classes.container}>
     <p className={classes.copyright}>
-      © 2019 - Kevin Brewer -
+      © {new Date().getFullYear()} Kevin Brewer
     </p>
     <SocialLink
-      to='https://github.com/appleJax'
-      className='fab fa-github'
+      to="https://github.com/appleJax"
+      className="fab fa-github"
       footer
     />
     <SocialLink
-      to='https://www.linkedin.com/in/kevin-brewer-profile'
-      className='fab fa-linkedin'
+      to="https://www.linkedin.com/in/kevin-brewer-profile"
+      className="fab fa-linkedin"
       footer
     />
     <SocialLink
-      to='mailto:kevnbrewer@gmail.com'
-      className='fas fa-envelope'
+      to="mailto:kevnbrewer@gmail.com"
+      className="fas fa-envelope"
       footer
     />
   </div>
+);
 
 const styles = (theme) => ({
   container: {
     ...Classes.flexRow,
-    justifyContent: 'center',
-    color: '#888',
+    justifyContent: "center",
+    color: "#888",
     background: Colors.secondary.night,
-    height: '60px',
-    marginTop: '20px',
-    width: '100vw'
+    height: "60px",
+    marginTop: "20px",
+    width: "100vw",
   },
   copyright: {
     color: Colors.darkGrey,
-    fontFamily: 'Roboto',
-    fontSize: '0.8em'
-  }
-})
+    fontFamily: "Roboto",
+    fontSize: "0.8em",
+  },
+});
 
-export default withStyles(styles)(Footer)
+export default withStyles(styles)(Footer);
--- src/components/common/TitleBar/Headshot.js
+++ src/components/common/TitleBar/Headshot.js
@@ -1,52 +1,44 @@
-import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import Classes from 'Styles/classes'
-import Colors from 'Styles/colors'
-import Typography from '@material-ui/core/Typography'
-import myPhoto from 'Images/me.jpeg'
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Classes from "Styles/classes";
+import Colors from "Styles/colors";
+import Typography from "@material-ui/core/Typography";
+import myPhoto from "Images/me.jpeg";
 
-const Headshot = ({ classes }) =>
+const Headshot = ({ classes }) => (
   <div className={classes.container}>
-    <img
-      alt='Me'
-      className={classes.photo}
-      src={myPhoto}
-    />
+    <img alt="Me" className={classes.photo} src={myPhoto} />
     <div className={classes.title}>
-      <Typography className={classes.titleLine} variant='h6'>
-        Kevin Brewer
-      </Typography>
-      <Typography className={classes.titleLine} variant='body1'>
-        Software Engineer
-      </Typography>
+      <Typography variant="h6">Kevin Brewer</Typography>
+      <Typography variant="body1">Software Engineer</Typography>
     </div>
   </div>
+);
 
 const styles = (theme) => ({
   container: {
     ...Classes.flexRow,
-    justifyContent: 'flex-start',
-    width: '33%',
-    [theme.breakpoints.down('sm')]: {
-      width: '50%'
+    justifyContent: "flex-start",
+    width: "33%",
+    [theme.breakpoints.down("sm")]: {
+      width: "50%",
     },
-    [theme.breakpoints.down('xs')]: {
-      width: '100%'
-    }
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
   },
   photo: {
-    borderRadius: '50%',
-    boxShadow: '3px 3px 15px rgba(0, 0, 0, 0.6)',
-    margin: '15px'
+    borderRadius: "50%",
+    boxShadow: "3px 3px 15px rgba(0, 0, 0, 0.6)",
+    margin: "15px",
   },
   title: {
     ...Classes.flexColumn,
     color: Colors.primary.night,
-    textShadow: `1px 1px 1px rgba(255, 255, 255, 0.2)`
+    textShadow: "1px 1px 1px rgba(255, 255, 255, 0.2)",
+    textAlign: "center",
+    whiteSpace: "nowrap",
   },
-  titleLine: {
-    textAlign: 'center'
-  }
-})
+});
 
-export default withStyles(styles)(Headshot)
+export default withStyles(styles)(Headshot);
